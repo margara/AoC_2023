cbf72be2532577933e7884933914301c1eee5790
Alessandro Margara <alessandro.margara@polimi.it>
2023-12-09 22:26:54 +0100

    Day 8 (non brute force :)!

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -26,27 +26,57 @@ fn parse_input() -> (String, HashMap<String, (String, String)>) {
 fn main() {
     let (directions, m) = parse_input();
 
+    // First part
     let mut count = 0;
-    let mut source = String::from("AAA");
+    let mut source = &String::from("AAA");
     loop {
         let d = directions.chars().nth(count % directions.len()).unwrap();
-        let (left, right) = m.get(&source).unwrap();
+        let (left, right) = m.get(source).unwrap();
         match d {
             'L' => {
-                source = left.clone();
+                source = left;
             }
             'R' => {
-                source = right.clone();
+                source = right;
             }
             _ => panic!("Unknown direction")
         }
         count += 1;
-        if source == "ZZZ" {
+        if *source == "ZZZ" {
             break;
         }
     }
     println!("Count: {}", count);
 
+    // Second part
+    let sources = m.keys().filter(|k| k.ends_with("A")).collect::<Vec<_>>();
+    let first_z = sources.iter().map(|s| {
+        let initial_source = (*s).clone();
+        let mut source = &initial_source;
+        let mut count = 0;
+        loop {
+            let d = directions.chars().nth(count % directions.len()).unwrap();
+            let (left, right) = m.get(source).unwrap();
+            match d {
+                'L' => {
+                    source = left;
+                }
+                'R' => {
+                    source = right;
+                }
+                _ => panic!("Unknown direction")
+            }
+            count += 1;
+            if source.ends_with("Z") {
+                break;
+            }
+        }
+        count
+    }).collect::<Vec<_>>();
+    let count = first_z.into_iter().reduce(|a, b| num::integer::lcm(a, b)).unwrap();
+    println!("Count: {}", count);
+
+    // Second part (brute force)
     let mut count = 0;
     let mut sources = m.keys().filter(|k| k.ends_with("A")).collect::<Vec<_>>();
     loop {
